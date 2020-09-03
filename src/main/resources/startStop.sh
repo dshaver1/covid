09fdcc823ab92e28072b21f1bc8dc2b16d4d3711
Daniel Shaver <daniel@dshaver.org>
2020-09-03 17:10:38 -0400

    Working on 1.0

--- src/main/resources/startStop.sh
+++ src/main/resources/startStop.sh
@@ -1,11 +1,11 @@
-pkill java
+sudo pkill java
 
-unzip /mnt/data/tmp/*.zip
-mv /mnt/data/tmp/covid/*.jar /mnt/data/covid
-cp /mnt/data/tmp/covid/static/* /mnt/data/apache/www/html/
+sudo unzip /mnt/data/tmp/*.zip
+sudo mv /mnt/data/tmp/covid/*.jar /mnt/data/covid
+sudo cp -r /mnt/data/tmp/covid/static/* /mnt/data/apache/www/html/
 
-rm -rf /mnt/data/tmp/covid
+sudo rm -rf /mnt/data/tmp/covid
 
-chmod +x /mnt/data/covid/*.jar
-java -Xmx1500m -Xms1500m -jar -Dspring.profiles.active=cloud /mnt/data/covid/covid-1.0.0-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo chmod +x /mnt/data/covid/*.jar
+sudo su - covid -c java -Xmx1500m -Xms1500m -jar -Dspring.profiles.active=cloud /mnt/data/covid/covid-1.0.0-SNAPSHOT.jar > /dev/null 2>&1 &
 tail -f /mnt/data/covid/logs/spring.log
