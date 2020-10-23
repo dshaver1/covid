ca700c1cbc322d969322b24c5aa2e2bf31c313c0
Daniel Shaver <daniel@dshaver.org>
2020-10-23 11:20:17 -0400

    Fixing java 15 issue with record

--- src/main/java/org/dshaver/covid/domain/CountyValuePair.java
+++ src/main/java/org/dshaver/covid/domain/CountyValuePair.java
@@ -1,3 +1,6 @@
 package org.dshaver.covid.domain;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record CountyValuePair(String county, Integer value) { }
