b5f9f4bfcfd3a0a55d1ccc6e79672dc5736095e8
Daniel Shaver <daniel@dshaver.org>
2020-10-27 14:10:33 -0400

    Minor reorg

---
+++ src/main/resources/static/covid/d3-utils.js
@@ -0,0 +1,225 @@
+
+
+function createSlider(data) {
+    let count = 0;
+
+    for (let value of Object.values(data)) {
+        if (value.length > 0) {
+            count++
+        }
+    }
+
+    count = (count - 1) * -1;
+
+    let tickValueArray = [];
+
+    for (let current = 0; current > count; current = current - 7) {
+        tickValueArray.push(current);
+    }
+
+    let sliderTime = d3
+        .sliderBottom()
+        //.min(-1 * (timeData.length - 1))
+        //.max(0)
+        .domain([count, 0])
+        .marks([count, 0])
+        .step(1)
+        //.ticks(5)
+        .width(350)
+        .tickValues(tickValueArray);
+
+    let gTime = d3
+        .select('div#slider-time')
+        .append('svg')
+        .attr('width', 400)
+        .attr('height', 100)
+        .append('g')
+        .attr('transform', 'translate(30,30)');
+
+    gTime.call(sliderTime);
+
+    return sliderTime;
+}
+
+function getLastElement(data) {
+    let keySet = Object.keys(data);
+    return data[keySet[keySet.length - 1]];
+}
+
+/**
+ * Gets the value from the provided array as if it were indexed from the end. So 0 gets the last element, and -data.length-1 gets the first element.
+ * -1 Gets the next to the last element.
+ */
+function getReverseIdxValue(data, idx) {
+    return data[data.length - 1 + idx];
+}
+
+function getLegendHeight(height, size, i) {
+    return height + 81 + (i * ((size + 2) + 5))
+}
+
+function toggleVisibilityExclusive(d) {
+    toggleVisibility(d, true);
+}
+
+function toggleVisibility(d) {
+    d.clazz.forEach(c => {
+        let selection = d3.selectAll("." + c);
+        if (selection && selection.data() && selection.data().length > 0) {
+            let currentVis = d3.selectAll("." + c).style("visibility");
+            console.log("Legend click! Toggling " + d.key + "... current visibility: " + currentVis);
+            let targetVis = "visible";
+            if (targetVis === currentVis) {
+                targetVis = "hidden";
+            }
+
+            d3.selectAll("." + c).transition().duration(200).style("visibility", targetVis);
+        }
+    });
+}
+
+function handleDeathMouseOver(d, i, d3This, color) {
+    d3This
+        .transition()
+        .duration(50)
+        .style("fill", color)
+        .attr("opacity", "0.2");
+    tip.show(d, i);
+}
+
+function handleDeathMouseOut(d, i, d3This, color) {
+    d3This
+        .transition()
+        .duration(200)
+        .style("fill", color)
+        .attr("opacity", "0");
+    tip.hide(d, i);
+}
+
+function dispatchMouseEvents(clazz, scaledX, prevNode, currentNode) {
+    currentNode[clazz] = d3.selectAll("." + clazz).filter(d => d.label === scaledX);
+
+    // First time entering?
+    if (!prevNode[clazz] && currentNode[clazz]) {
+        //console.log("First time? scaledX: " + scaledX);
+        prevNode[clazz] = currentNode[clazz];
+        currentNode[clazz].dispatch("click");
+    }
+
+    // Exited past the range of available data
+    if (!currentNode[clazz].data()[0] || currentNode[clazz].data().length === 0) {
+        //console.log("Exit past available? prevLabel: " + prevNode[clazz].data()[0].label + " scaledX: " + scaledX);
+        prevNode[clazz].dispatch("mouseout");
+    }
+
+    // Most common case, moving from 1 good date to the next.
+    if ((prevNode[clazz].data().length > 0 && currentNode[clazz].data().length) && (prevNode[clazz].data()[0].label !== currentNode[clazz].data()[0].label)) {
+        //console.log("Good transition? prevLabel: " + prevNode[clazz].data()[0].label + " currLabel: " + currentNode[clazz].data()[0].label + " scaledX: " + scaledX);
+        prevNode[clazz].dispatch("mouseout");
+        currentNode[clazz].dispatch("click");
+        prevNode[clazz] = currentNode[clazz];
+    } else {
+        //console.log("Doing nothing! prevLabel: " + prevNode[clazz].data()[0].label + " scaledX: " + scaledX);
+    }
+}
+
+function sumTimeSeries(data) {
+    return Object.values(data).map(d => +d).filter(d => !isNaN(d)).reduce((d1, d2) => d1 + d2)
+}
+
+function getCountyFromUrl(url) {
+    let selectedCounty = new URLSearchParams(url).get("county");
+
+    if (!selectedCounty) {
+        selectedCounty = "georgia";
+    }
+
+    return selectedCounty;
+}
+
+function constructCountyUrl(type, county) {
+    return "reports/v2/csv/" + county + "/" + type + "_" + county + ".csv"
+}
+
+function getFilteredCountyName(inputCounty) {
+    let filteredCounties = allCounties.filter(county => county.toLowerCase() === inputCounty.toLowerCase());
+    if (filteredCounties && filteredCounties.length === 1) {
+        return filteredCounties[0];
+    } else {
+        return undefined;
+    }
+}
+
+const earliestDate = new Date("2020-05-14");
+
+function getDateAtMouse(mouse, xScale) {
+    let index = Math.floor(((mouse[0] - xScale(xScale.domain()[0])) / xScale.step()));
+    let scaledX = xScale.domain()[index];
+    if (!scaledX) {
+        scaledX = xScale.domain()[xScale.domain().length - 1];
+    }
+
+    //console.log("scaledX " + scaledX + " @ mouse " + mouse[0]);
+
+    return scaledX;
+}
+
+function handleMouseClick(scaledX, xScale) {
+    let targetDate = scaledX;
+
+    if (new Date(scaledX) < earliestDate) {
+        targetDate = "2020-05-13";
+    }
+
+    let event = new CustomEvent('newDateEvent', {detail: {label: targetDate}});
+
+    dispatchEvent(event);
+
+    d3.selectAll(".clicked-mouse-date")
+        .attr("opacity", "1")
+        .text(targetDate)
+        .attr("y", xScale(targetDate) + (xScale.step() / 2));
+
+    d3.select(".clicked-mouse-date-line")
+        .attr("opacity", "1")
+        .attr("x1", xScale(targetDate) + (xScale.step() / 2))
+        .attr("x2", xScale(targetDate) + (xScale.step() / 2));
+
+    //d3.selectAll(".hover-effects").style("opacity", "0");
+}
+
+function handleDblClick(scaledX) {
+    let targetDate = scaledX;
+
+    if (new Date(scaledX) < earliestDate) {
+        targetDate = "2020-05-13";
+    }
+
+    let event = new CustomEvent('newFocusEvent', {detail: {label: targetDate, focus: "caseDeltas"}});
+
+    dispatchEvent(event);
+}
+
+function applyDateOffset(date, offset) {
+    let dateObj = this.convertDate(date);
+    dateObj.setDate(dateObj.getDate() - offset);
+    return getFormattedDate(dateObj);
+}
+
+function getFormattedDate(date) {
+    let year = date.getFullYear();
+    let month = (1 + date.getMonth()).toString().padStart(2, '0');
+    let day = date.getDate().toString().padStart(2, '0');
+
+    return year + '-' + month + '-' + day;
+}
+
+/**
+ * Convert a date string in 2020-10-10T101300 format into a Date() object.
+ */
+function convertDate(dateString) {
+    let filteredDateString = dateString.slice(0, 15) + ":" + dateString.slice(15 + Math.abs(0))
+    filteredDateString = filteredDateString.slice(0, 13) + ":" + filteredDateString.slice(13 + Math.abs(0));
+    filteredDateString = filteredDateString.replace("T", " ");
+    return new Date(filteredDateString);
+}
--- src/main/resources/static/covid/epicurve3.js
+++ src/main/resources/static/covid/epicurve3.js
@@ -1,5 +1,6 @@
 const allCounties = ["Georgia", "Unknown", "Non-Georgia-Resident", "Healthcare", "Appling", "Atkinson", "Bacon", "Baker", "Baldwin", "Banks", "Barrow", "Bartow", "Ben-Hill", "Berrien", "Bibb", "Bleckley", "Brantley", "Brooks", "Bryan", "Bulloch", "Burke", "Butts", "Calhoun", "Camden", "Candler", "Carroll", "Catoosa", "Charlton", "Chatham", "Chattahoochee", "Chattooga", "Cherokee", "Clarke", "Clay", "Clayton", "Clinch", "Cobb", "Coffee", "Colquitt", "Columbia", "Cook", "Coweta", "Crawford", "Crisp", "Dade", "Dawson", "DeKalb", "Decatur", "Dodge", "Dooly", "Dougherty", "Douglas", "Early", "Echols", "Effingham", "Elbert", "Emanuel", "Evans", "Fannin", "Fayette", "Floyd", "Forsyth", "Franklin", "Fulton", "Gilmer", "Glascock", "Glynn", "Gordon", "Grady", "Greene", "Gwinnett", "Habersham", "Hall", "Hancock", "Haralson", "Harris", "Hart", "Heard", "Henry", "Houston", "Irwin", "Jackson", "Jasper", "JeffDavis", "Jefferson", "Jenkins", "Johnson", "Jones", "Lamar", "Lanier", "Laurens", "Lee", "Liberty", "Lincoln", "Long", "Lowndes", "Lumpkin", "Macon", "Madison", "Marion", "McDuffie", "McIntosh", "Meriwether", "Miller", "Mitchell", "Monroe", "Montgomery", "Morgan", "Murray", "Muscogee", "Newton", "Oconee", "Oglethorpe", "Paulding", "Peach", "Pickens", "Pierce", "Pike", "Polk", "Pulaski", "Putnam", "Quitman", "Rabun", "Randolph", "Richmond", "Rockdale", "Schley", "Screven", "Seminole", "Spalding", "Stephens", "Stewart", "Sumter", "Talbot", "Taliaferro", "Tattnall", "Taylor", "Telfair", "Terrell", "Thomas", "Tift", "Toombs", "Towns", "Treutlen", "Troup", "Turner", "Twiggs", "Union", "Upson", "Walker", "Walton", "Ware", "Warren", "Washington", "Wayne", "Webster", "Wheeler", "White", "Whitfield", "Wilcox", "Wilkes", "Wilkinson", "Worth"];
-
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const monthDates = ["2020-01-01", "2020-02-01", "2020-03-01", "2020-04-01", "2020-05-01", "2020-06-01", "2020-07-01", "2020-08-01", "2020-09-01", "2020-10-01", "2020-11-01", "2020-12-01"];
 
 class Epicurve {
 
@@ -13,12 +14,6 @@ class Epicurve {
         this.yAxis = yAxis;
         this.yScale2 = yScale2;
         this.yAxis2 = yAxis2;
-        this.prevNode = {};
-        this.currentNode = {};
-        this.monthNames = ["January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        ];
-        this.monthDates = ["2020-01-01", "2020-02-01", "2020-03-01", "2020-04-01", "2020-05-01", "2020-06-01", "2020-07-01", "2020-08-01", "2020-09-01", "2020-10-01", "2020-11-01", "2020-12-01"];
 
         this.globalDuration = 200;
         let selectedData = this.svg.selectAll(".prelim-region").data(["2020-08-08"]);
@@ -205,6 +200,9 @@ class Epicurve {
                 let d = d3.selectAll(".casedeltabar").filter(d => d.label === d3.select("text.mouse-date").text()).data()[0];
                 constructorThis.updateMouseOverLine(d);
             })
+            .on('dblclick', function () {
+                handleDblClick(getDateAtMouse(d3.mouse(this), xScale));
+            })
             .on('wheel', function () {
                 let wheelDelta = d3.event.deltaY;
                 let direction = wheelDelta < 0 ? '-1' : '1';
@@ -225,14 +223,6 @@ class Epicurve {
 
                 if (targetDate) {
                     handleMouseClick(targetDate, xScale);
-
-                    /*                    let clickedD = d3.selectAll(".casedeltabar").filter(d => d.label === d3.select("text.clicked-mouse-date").text()).data()[0];
-                                        let mouseD = d3.selectAll(".casedeltabar").filter(d => d.label === d3.select("text.mouse-date").text()).data()[0];
-                                        if (!mouseD || (clickedD && new Date(clickedD.label) <= new Date(mouseD.label))) {
-                                            constructorThis.updateMouseOverLine(clickedD);
-                                        } else {
-                                            constructorThis.updateMouseOverLine(mouseD);
-                                        }*/
                 }
             })
             .call(drag);
@@ -241,7 +231,7 @@ class Epicurve {
     }
 
     initDateLines() {
-        this.svg.selectAll(".month-lines").data(this.monthDates)
+        this.svg.selectAll(".month-lines").data(monthDates)
             .join(enter => {
                     enter
                         .append("line")
@@ -267,7 +257,7 @@ class Epicurve {
                     exit.remove();
                 });
 
-        this.svg.selectAll(".month-text").data(this.monthDates)
+        this.svg.selectAll(".month-text").data(monthDates)
             .join(enter => {
                     enter
                         .append("text")
@@ -275,7 +265,7 @@ class Epicurve {
                         .attr("x", -this.height - 7)
                         .attr("y", d => this.xScale(d) + this.xScale.bandwidth())
                         .style("visibility", d => !this.xScale(d) ? "hidden" : "visible")
-                        .text(d => this.monthNames[new Date(d).getMonth() + 1])
+                        .text(d => monthNames[new Date(d).getMonth() + 1])
                         .attr("text-anchor", "end")
                         .style("font", "10px sans-serif")
                         .style("alignment-baseline", "top")
@@ -289,36 +279,6 @@ class Epicurve {
                 exit => {
                     exit.remove();
                 });
-
-
-        /*
-
-        this.svg.selectAll("." + clazz).data(data)
-            .join(enter => {
-                    let rect = enter.append("rect")
-                        .style("shape-rendering", "crispEdges")
-                        .attr("class", clazz)
-                        .attr("width", this.xScale.bandwidth())
-                        .style("fill", d => yCallback(d) > 0 ? color : highlightColor)
-                        .attr("x", d => this.xScale(xCallback(d)))
-                        .attr("y", this.yScale(0))
-                        .attr("height", 0);
-
-                    rect.transition().duration(this.globalDuration)
-                        .attr("y", d => yCallback(d) > 0 ? this.yScale(yCallback(d)) : this.yScale(0))
-                        .attr("height", d => Math.abs(this.yScale(yCallback(d)) - this.yScale(0)));
-                },
-                update => update
-                    .transition().duration(this.globalDuration)
-                    .attr("y", d => yCallback(d) > 0 ? this.yScale(yCallback(d)) : this.yScale(0))
-                    .attr("height", d => Math.abs(this.yScale(yCallback(d)) - this.yScale(0)))
-                    .style("fill", d => yCallback(d) > 0 ? color : highlightColor),
-                exit => exit
-                    .transition().duration(this.globalDuration)
-                    .attr("y", this.yScale(0))
-                    .attr("height", 0)
-                    .remove());
-         */
     }
 
     /**
@@ -557,7 +517,7 @@ class Epicurve {
                         .attr("fill", color)
                         .attr("opacity", "0.5")
                         .attr('r', 2)
-                        .style("visibility", isVisible ? "visible" : "hidden")
+                        .style("visibility", isVisible ? "hidden" : "hidden")
                         .on('click', function () {
                             d3.select(this).attr("r", 5).attr("isHover", "1");
                         })
@@ -1425,218 +1385,6 @@ class Epicurve {
 
         return correlationCoefficient;
     }
-}
-
-function createSlider(data) {
-    let count = 0;
-
-    for (let value of Object.values(data)) {
-        if (value.length > 0) {
-            count++
-        }
-    }
-
-    count = (count - 1) * -1;
-
-    let tickValueArray = [];
-
-    for (let current = 0; current > count; current = current - 7) {
-        tickValueArray.push(current);
-    }
-
-    let sliderTime = d3
-        .sliderBottom()
-        //.min(-1 * (timeData.length - 1))
-        //.max(0)
-        .domain([count, 0])
-        .marks([count, 0])
-        .step(1)
-        //.ticks(5)
-        .width(350)
-        .tickValues(tickValueArray);
-
-    let gTime = d3
-        .select('div#slider-time')
-        .append('svg')
-        .attr('width', 400)
-        .attr('height', 100)
-        .append('g')
-        .attr('transform', 'translate(30,30)');
-
-    gTime.call(sliderTime);
-
-    return sliderTime;
-}
-
-function getLastElement(data) {
-    let keySet = Object.keys(data);
-    return data[keySet[keySet.length - 1]];
-}
-
-/**
- * Gets the value from the provided array as if it were indexed from the end. So 0 gets the last element, and -data.length-1 gets the first element.
- * -1 Gets the next to the last element.
- */
-function getReverseIdxValue(data, idx) {
-    return data[data.length - 1 + idx];
-}
-
-function getLegendHeight(height, size, i) {
-    return height + 81 + (i * ((size + 2) + 5))
-}
-
-function toggleVisibilityExclusive(d) {
-    toggleVisibility(d, true);
-}
-
-function toggleVisibility(d) {
-    d.clazz.forEach(c => {
-        let selection = d3.selectAll("." + c);
-        if (selection && selection.data() && selection.data().length > 0) {
-            let currentVis = d3.selectAll("." + c).style("visibility");
-            console.log("Legend click! Toggling " + d.key + "... current visibility: " + currentVis);
-            let targetVis = "visible";
-            if (targetVis === currentVis) {
-                targetVis = "hidden";
-            }
-
-            d3.selectAll("." + c).transition().duration(200).style("visibility", targetVis);
-        }
-    });
-}
-
-function handleDeathMouseOver(d, i, d3This, color) {
-    d3This
-        .transition()
-        .duration(50)
-        .style("fill", color)
-        .attr("opacity", "0.2");
-    tip.show(d, i);
-}
-
-function handleDeathMouseOut(d, i, d3This, color) {
-    d3This
-        .transition()
-        .duration(200)
-        .style("fill", color)
-        .attr("opacity", "0");
-    tip.hide(d, i);
-}
-
-function dispatchMouseEvents(clazz, scaledX, prevNode, currentNode) {
-    currentNode[clazz] = d3.selectAll("." + clazz).filter(d => d.label === scaledX);
-
-    // First time entering?
-    if (!prevNode[clazz] && currentNode[clazz]) {
-        //console.log("First time? scaledX: " + scaledX);
-        prevNode[clazz] = currentNode[clazz];
-        currentNode[clazz].dispatch("click");
-    }
-
-    // Exited past the range of available data
-    if (!currentNode[clazz].data()[0] || currentNode[clazz].data().length === 0) {
-        //console.log("Exit past available? prevLabel: " + prevNode[clazz].data()[0].label + " scaledX: " + scaledX);
-        prevNode[clazz].dispatch("mouseout");
-    }
-
-    // Most common case, moving from 1 good date to the next.
-    if ((prevNode[clazz].data().length > 0 && currentNode[clazz].data().length) && (prevNode[clazz].data()[0].label !== currentNode[clazz].data()[0].label)) {
-        //console.log("Good transition? prevLabel: " + prevNode[clazz].data()[0].label + " currLabel: " + currentNode[clazz].data()[0].label + " scaledX: " + scaledX);
-        prevNode[clazz].dispatch("mouseout");
-        currentNode[clazz].dispatch("click");
-        prevNode[clazz] = currentNode[clazz];
-    } else {
-        //console.log("Doing nothing! prevLabel: " + prevNode[clazz].data()[0].label + " scaledX: " + scaledX);
-    }
-}
-
-function sumTimeSeries(data) {
-    return Object.values(data).map(d => +d).filter(d => !isNaN(d)).reduce((d1, d2) => d1 + d2)
-}
-
-function getCountyFromUrl(url) {
-    let selectedCounty = new URLSearchParams(url).get("county");
-
-    if (!selectedCounty) {
-        selectedCounty = "georgia";
-    }
-
-    return selectedCounty;
-}
-
-function constructCountyUrl(type, county) {
-    return "reports/v2/csv/" + county + "/" + type + "_" + county + ".csv"
-}
-
-function getFilteredCountyName(inputCounty) {
-    let filteredCounties = allCounties.filter(county => county.toLowerCase() === inputCounty.toLowerCase());
-    if (filteredCounties && filteredCounties.length === 1) {
-        return filteredCounties[0];
-    } else {
-        return undefined;
-    }
-}
-
-const earliestDate = new Date("2020-05-14");
-
-function getDateAtMouse(mouse, xScale) {
-    let index = Math.floor(((mouse[0] - xScale(xScale.domain()[0])) / xScale.step()));
-    let scaledX = xScale.domain()[index];
-    if (!scaledX) {
-        scaledX = xScale.domain()[xScale.domain().length - 1];
-    }
-
-    //console.log("scaledX " + scaledX + " @ mouse " + mouse[0]);
-
-    return scaledX;
-}
-
-function handleMouseClick(scaledX, xScale) {
-    let targetDate = scaledX;
-
-    if (new Date(scaledX) < earliestDate) {
-        targetDate = "2020-05-13";
-    }
-
-    let event = new CustomEvent('newDateEvent', {detail: {label: targetDate}});
-
-    dispatchEvent(event);
-
-    d3.selectAll(".clicked-mouse-date")
-        .attr("opacity", "1")
-        .text(targetDate)
-        .attr("y", xScale(targetDate) + (xScale.step() / 2));
-
-    d3.select(".clicked-mouse-date-line")
-        .attr("opacity", "1")
-        .attr("x1", xScale(targetDate) + (xScale.step() / 2))
-        .attr("x2", xScale(targetDate) + (xScale.step() / 2));
-
-    //d3.selectAll(".hover-effects").style("opacity", "0");
-}
-
-function applyDateOffset(date, offset) {
-    let dateObj = this.convertDate(date);
-    dateObj.setDate(dateObj.getDate() - offset);
-    return getFormattedDate(dateObj);
-}
-
-function getFormattedDate(date) {
-    let year = date.getFullYear();
-    let month = (1 + date.getMonth()).toString().padStart(2, '0');
-    let day = date.getDate().toString().padStart(2, '0');
-
-    return year + '-' + month + '-' + day;
-}
-
-/**
- * Convert a date string in 2020-10-10T101300 format into a Date() object.
- */
-function convertDate(dateString) {
-    let filteredDateString = dateString.slice(0, 15) + ":" + dateString.slice(15 + Math.abs(0))
-    filteredDateString = filteredDateString.slice(0, 13) + ":" + filteredDateString.slice(13 + Math.abs(0));
-    filteredDateString = filteredDateString.replace("T", " ");
-    return new Date(filteredDateString);
 }
 
 d3.selection.prototype.moveToFront = function () {
